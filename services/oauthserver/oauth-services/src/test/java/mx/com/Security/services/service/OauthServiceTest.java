75a8449eba28213f668687d990ca82e65ad7a5ff
Javier Rodriguez <francisco.rodriguez@axity.com>
2020-07-12 16:23:36 -0500

    Se agrega Api Manager y OauthServer

---
+++ services/oauthserver/oauth-services/src/test/java/mx/com/Security/services/service/OauthServiceTest.java
@@ -0,0 +1,13 @@
+package mx.com.Security.services.service;
+
+import mx.com.Security.services.BaseTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OauthServiceTest extends BaseTest {
+
+    @Test
+    public void emptyTest() {
+        Assert.assertTrue(true);
+    }
+}
