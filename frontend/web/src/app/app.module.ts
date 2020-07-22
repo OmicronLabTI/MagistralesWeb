2dd4d0c52dca41f27da1b146d1a45920fba6fab3
benny alva <bennytescha@gmail.com>
2020-07-21 19:58:03 -0500

    deleted comments from app.module

--- frontend/web/src/app/app.module.ts
+++ frontend/web/src/app/app.module.ts
@@ -4,18 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './pages/home/home.component';
 import { MATERIAL_COMPONENTS } from './app.material';
-import { LoginComponent } from './pages/login/login.component';
 import { APP_PROVIDERS } from './app.providers';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { TestComponent } from './components/test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    //TestComponent
   ],
   imports: [
     BrowserModule,
