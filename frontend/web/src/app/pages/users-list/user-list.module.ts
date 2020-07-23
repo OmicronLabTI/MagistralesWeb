dc123665c218f4f37ce6e12b8f57e9df30f8f68f
benny alva <bennytescha@gmail.com>
2020-07-23 15:46:29 -0500

    maquetado

---
+++ frontend/web/src/app/pages/users-list/user-list.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserListRoutingModule } from './user-list-routing.module';
+import {UsersListComponent} from "./users-list.component";
+
+
+@NgModule({
+  declarations: [UsersListComponent],
+  imports: [
+    CommonModule,
+    UserListRoutingModule
+  ]
+})
+export class UserListModule { }
