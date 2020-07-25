f532a12c981906e7a308216fd6abdac0b0e0e46e
benny alva <bennytescha@gmail.com>
2020-07-25 17:07:39 -0500

    se agrega user list page

--- frontend/web/src/app/pages/users-list/users-list.component.ts
+++ frontend/web/src/app/pages/users-list/users-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserList} from "../../model/http/userList";
 
 @Component({
   selector: 'app-users-list',
@@ -6,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-list.component.scss']
 })
 export class UsersListComponent implements OnInit {
-
+  checkedGeneral = false;
+  listUserData: UserList [] = [
+    {
+      "id": 0,
+      "name": "benny",
+      "lastName": "alvarado",
+      "rol": "administrador",
+      "status": "inactivo"
+    },
+    {
+      "id": 1,
+      "name": "berenice",
+      "lastName": "amezaga",
+      "rol": "administrador",
+      "status": "activo"
+    },
+    {
+      "id": 2,
+      "name": "irma",
+      "lastName": "hernandez",
+      "rol": "gfb",
+      "status": "inactivo"
+    },
+    {
+      "id": 3,
+      "name": "erika",
+      "lastName": "villalba",
+      "rol": "administrador",
+      "status": "activo"
+    },
+    {
+      "id": 4,
+      "name": "mayte",
+      "lastName": "rivera",
+      "rol": "qfb",
+      "status": "inactivo"
+    }
+  ];
   constructor() { }
-
+  checkGeneralEvent(){
+    console.log('value checked general: ', this.checkedGeneral)
+    this.checkedGeneral = !this.checkedGeneral;
+  }
   ngOnInit() {
   }
 
