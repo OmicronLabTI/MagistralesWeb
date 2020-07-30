a6584a53675ee1a4c176d9d0ebe6b29d9119560a
Diego Cárcamo <39307881+diegoaxity@users.noreply.github.com>
2020-07-30 15:34:15 -0500

    add params to http get

--- frontend/web/src/app/services/consume.service.ts
+++ frontend/web/src/app/services/consume.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, from } from 'rxjs';
-import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
 import { DataService } from './data.service';
 
 @Injectable({
@@ -10,7 +10,7 @@ export class ConsumeService {
 
   constructor(private http: HttpClient, private dataService: DataService) { }
 
-  httpGet<T>(url: string, headers?) {
+  httpGet<T>(url: string, params?, headers?) {
     let objHeaders = new HttpHeaders();
     if (headers) {
       Object.keys(headers).forEach((key) => {
@@ -18,9 +18,16 @@ export class ConsumeService {
       });
     }
 
+    let objParams = new HttpParams();
+    if (params) {
+      Object.keys(params).forEach((key) => {
+        objParams = objParams.append(key, params[key]);
+      });
+    }
+
     this.dataService.setIsLoading(true);
     return new Observable<T>(observer => {
-      this.http.get<any>(url, { headers: objHeaders })
+      this.http.get<any>(url, { headers: objHeaders, params: objParams })
         .subscribe(response => {
           observer.next(response);
           observer.complete();
