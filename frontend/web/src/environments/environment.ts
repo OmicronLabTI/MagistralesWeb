4ee4ce615a8f9d42792b3433e768b564b073e73a
benny alva <bennytescha@gmail.com>
2020-07-31 15:04:00 -0500

    server ip

--- frontend/web/src/environments/environment.ts
+++ frontend/web/src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
 
-  //baseUrl: 'http://localhost:8000',//prod
-  baseUrl: 'https://febf9d417342.ngrok.io', //dev
+  baseUrl: 'http://localhost:8000',//prod
+  //baseUrl: 'https://febf9d417342.ngrok.io', //dev
   baseUrlLogin:'http://localhost:8090', //dev
   baseUrlUsers:'http://localhost:5101', //dev
   baseUrlCatalogs:'http://localhost:9010', //dev
