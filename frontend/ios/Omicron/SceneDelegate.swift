0cbb256a7251597f99d7c471a4f8af45581ca274
Sergio Flores Ramírez <serchfr890123@gmail.com>
2020-07-31 01:05:24 -0500

    Se coloca rootView SplitView

--- frontend/ios/Omicron/SceneDelegate.swift
+++ frontend/ios/Omicron/SceneDelegate.swift
@@ -19,19 +19,25 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         
-        if let windowScene = scene as? UIWindowScene {
+        if scene is UIWindowScene {
             //UserDefaults.standard.bool(forKey: "SessionActive")
             if UserDefaults.standard.bool(forKey: UsersDefaultsConstants.isSessionActive) {
-                let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-                let inboxViewController = mainStoryboard.instantiateViewController(withIdentifier: ViewControllerIdentifiers.inboxViewController) as! InboxViewController
-                let nav = UINavigationController(rootViewController: inboxViewController)
-                let window = UIWindow(windowScene: windowScene)
-                window.rootViewController = nav
-                self.window = window
-                window.makeKeyAndVisible() }
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let initialViewController = storyboard.instantiateViewController(withIdentifier: "SplitViewController")
+                self.window?.rootViewController = initialViewController
+                self.window?.makeKeyAndVisible()
+//                let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+//                let inboxViewController = mainStoryboard.instantiateViewController(withIdentifier: "SplitViewController") as! UISplitViewController
+//                let nav = UINavigationController(rootViewController: inboxViewController)
+//                let window = UIWindow(windowScene: windowScene)
+//                window.rootViewController = nav
+//                self.window = window
+//                window.makeKeyAndVisible()
+                
             }
+        }
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
@@ -59,7 +65,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
 
 }
 
