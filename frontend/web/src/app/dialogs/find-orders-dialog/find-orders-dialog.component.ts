8c60e019ee8cb01d6edfd35a39662ab0cda06453
benny alva <bennytescha@gmail.com>
2020-08-05 16:07:37 -0500

    create modal find orders

---
+++ frontend/web/src/app/dialogs/find-orders-dialog/find-orders-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-find-orders-dialog',
+  templateUrl: './find-orders-dialog.component.html',
+  styleUrls: ['./find-orders-dialog.component.scss']
+})
+export class FindOrdersDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
