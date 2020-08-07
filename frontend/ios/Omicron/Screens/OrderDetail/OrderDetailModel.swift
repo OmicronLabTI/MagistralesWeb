2a74187731f2d37787c7b642c231c71e80f08b2f
Sergio Flores Ramírez <serchfr890123@gmail.com>
2020-08-07 12:54:20 -0500

    Se añade pantalla en blanco para detalle de la orden

---
+++ frontend/ios/Omicron/Screens/OrderDetail/OrderDetailModel.swift
@@ -0,0 +1,9 @@
+//
+//  OrderDetailModel.swift
+//  Omicron
+//
+//  Created by Axity on 07/08/20.
+//  Copyright © 2020 Diego Cárcamo. All rights reserved.
+//
+
+import Foundation
---
+++ frontend/ios/Omicron/Screens/OrderDetail/OrderDetailViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  OrderDetailViewModel.swift
+//  Omicron
+//
+//  Created by Axity on 07/08/20.
+//  Copyright © 2020 Diego Cárcamo. All rights reserved.
+//
+
+import Foundation
