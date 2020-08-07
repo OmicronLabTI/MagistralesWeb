0aefd5f39e7ce7de4a8065255dd00acfeec876c1
Sergio Flores Ramírez <serchfr890123@gmail.com>
2020-08-07 15:09:47 -0500

    Se realizan conecciones de componentes entre el controller y viewModel

--- frontend/ios/Omicron/Screens/OrderDetail/OrderDetailViewController.swift
+++ frontend/ios/Omicron/Screens/OrderDetail/OrderDetailViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class OrderDetailViewController: UIViewController {
 
+    // Outlets
+    
+    @IBOutlet weak var nameStatusLabel: UILabel!
+    @IBOutlet weak var processButton: UIButton!
+    @IBOutlet weak var finishedButton: UIButton!
+    @IBOutlet weak var addComponentButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var seeLotsButton: UIButton!
+    @IBOutlet weak var penddingButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var codeLabel: UILabel!
+    @IBOutlet weak var codeDescriptionLabel: UILabel!
+    @IBOutlet weak var documentBaseLabel: UILabel!
+    @IBOutlet weak var documentBaseDescriptionLabel: UILabel!
+    @IBOutlet weak var containerLabel: UILabel!
+    @IBOutlet weak var containerDescriptionLabel: UILabel!
+    @IBOutlet weak var tagLabel: UILabel!
+    @IBOutlet weak var tagDescriptionLabel: UILabel!
+    @IBOutlet weak var sumFormulaLabel: UILabel!
+    @IBOutlet weak var sumFormulaDescriptionLabel: UILabel!
+    @IBOutlet weak var quantityPlannedLabel: UILabel!
+    @IBOutlet weak var quantityPlannedDescriptionLabel: UILabel!
+    @IBOutlet weak var startDateLabel: UILabel!
+    @IBOutlet weak var startDateDescriptionLabel: UILabel!
+    @IBOutlet weak var finishedDateLabel: UILabel!
+    @IBOutlet weak var finishedDateDescriptionLabel: UILabel!
+    @IBOutlet weak var productLabel: UILabel!
+    @IBOutlet weak var productDescritionLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
