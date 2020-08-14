52ec764668da73f0448c3abfc4d2ffadf0413bfd
gaboxlive <gabriel.cruz@axity.com>
2020-08-13 20:35:20 -0500

    Actualizacion

--- frontend/web/src/environments/environment.prod.ts
+++ frontend/web/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   // baseUrl: 'http://localhost:8000', // prod
-  baseUrl: 'https://30062a3026d6.ngrok.io', // dev
+  baseUrl: 'https://7cd7583c1ce0.ngrok.io', // dev
   baseUrlLogin: 'http://localhost:8090', // dev
   baseUrlUsers: 'http://localhost:5101', // dev
   baseUrlCatalogs: 'http://localhost:9010', // dev
