aab7e301e825cc213472b985d59f39bd10360442
Gustavo Ramirez <gustavo.ramirez@axity.com>
2020-08-17 19:17:22 -0500

    Se corrige archivo de environmtents

--- frontend/web/src/environments/environment.ts
+++ frontend/web/src/environments/environment.ts
@@ -4,11 +4,7 @@
 
 export const environment = {
     production: false,
-    // baseUrl: 'https://7ce4f9b67b4a.ngrok.io', // prod
     baseUrl: 'http://192.168.0.101:8000', // dev
-    baseUrlLogin: 'http://192.168.0.101:8090', // dev
-    baseUrlUsers: 'http://192.168.0.101:5101', // dev
-    baseUrlCatalogs: 'http://192.168.0.101:9010', // dev
     apiLogin: '/api/oauth/oauthrs',
     apiCatalogs: '/api/catalogos',
     apiUsers: '/api/usuarios',
