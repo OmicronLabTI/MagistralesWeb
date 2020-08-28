fc3cc8c35ae54a936f560ac23c13f53e1224bc08
benny alva <bennytescha@gmail.com>
2020-08-28 17:32:14 -0500

    resolved issues and unit test

---
+++ frontend/web/src/mocks/pedidosListMock.ts
@@ -0,0 +1,69 @@
+export const PedidosListMock = {
+    code: 200,
+    comments: 118,
+    exceptionMessage: null,
+    success: true,
+    userError: null,
+    response: [
+        {
+            docNum: 60021,
+            cliente: 'VENTA AL PUBLICO',
+            codigo: 'C00100',
+            medico: 'VENTA AL PUBLICO',
+            asesorName: 'OBIOTIX ONLINE',
+            fechaInicio: '30/07/2020',
+            fechaFin: '09/08/2020',
+            pedidoStatus: 'Abierto',
+            qfb: 'Armando Hoyos',
+            isChecked: false
+        },
+        {
+            docNum: 60022,
+            cliente: 'ABELARDO LEAL DUMONT',
+            codigo: 'C00826',
+            medico: 'ABELARDO LEAL DUMONT',
+            asesorName: 'CC1 - SELENE ESCAMILLA',
+            fechaInicio: '13/08/2020',
+            fechaFin: '23/08/2020',
+            pedidoStatus: 'Planificado',
+            qfb: 'Armando Hoyos',
+            isChecked: false
+        },
+        {
+            docNum: 60023,
+            cliente: 'ABELARDO LEAL DUMONT',
+            codigo: 'C00826',
+            medico: 'ABELARDO LEAL DUMONT',
+            asesorName: 'CC1 - SELENE ESCAMILLA',
+            fechaInicio: '13/08/2020',
+            fechaFin: '23/08/2020',
+            pedidoStatus: 'Liberado',
+            qfb: 'Armando Hoyos',
+            isChecked: false
+        },
+        {
+            docNum: 60024,
+            cliente: 'ABELARDO LEAL DUMONT',
+            codigo: 'C00826',
+            medico: 'ABELARDO LEAL DUMONT',
+            asesorName: 'CC1 - SELENE ESCAMILLA',
+            fechaInicio: '13/08/2020',
+            fechaFin: '23/08/2020',
+            pedidoStatus: 'Cancelado',
+            qfb: 'Armando Hoyos',
+            isChecked: false
+        },
+        {
+            docNum: 60025,
+            cliente: 'ABELARDO LEAL DUMONT',
+            codigo: 'C00826',
+            medico: 'ABELARDO LEAL DUMONT',
+            asesorName: 'CC1 - SELENE ESCAMILLA',
+            fechaInicio: '13/08/2020',
+            fechaFin: '23/08/2020',
+            pedidoStatus: 'En proceso',
+            qfb: 'Armando Hoyos',
+            isChecked: false
+        }
+    ]
+};
