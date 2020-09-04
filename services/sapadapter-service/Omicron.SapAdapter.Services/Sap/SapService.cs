1b5a555b7622705473198d095d47ece60e87691b
Gustavo Ramirez <gustavo.ramirez@axity.com>
2020-09-04 18:33:53 -0500

    Se corrige el round

--- services/sapadapter-service/Omicron.SapAdapter.Services/Sap/SapService.cs
+++ services/sapadapter-service/Omicron.SapAdapter.Services/Sap/SapService.cs
@@ -107,7 +107,7 @@ namespace Omicron.SapAdapter.Services.Sap
 
                 x.Status = userOrder.Status;
                 x.Status = x.Status.Equals(ServiceConstants.Proceso) ? ServiceConstants.EnProceso : x.Status;
-                x.FechaOfFin = x.Status.Equals(ServiceConstants.Terminado) ? userOrder.FinishDate : string.Empty;
+                x.FechaOfFin = userOrder.FinishDate;
                 x.PedidoStatus = pedido == null ? ServiceConstants.Abierto : pedido.Status;
             });
 
@@ -310,12 +310,15 @@ namespace Omicron.SapAdapter.Services.Sap
                 var totalBatches = batches.Any() ? batches.Sum(y => y.CantidadSeleccionada) : 0;
                 double.TryParse(totalBatches.ToString(), out var doubleTotalBathches);
 
+                totalNecesario = Math.Round(totalNecesario, 6);
+                doubleTotalBathches = Math.Round(doubleTotalBathches, 6);
+
                 listToReturn.Add(new BatchesComponentModel
                 {
                     Almacen = x.Warehouse,
                     CodigoProducto = x.ProductId,
                     DescripcionProducto = x.Description,
-                    TotalNecesario = totalNecesario - doubleTotalBathches,
+                    TotalNecesario = Math.Round(totalNecesario - doubleTotalBathches, 6),
                     TotalSeleccionado = doubleTotalBathches,
                     Lotes = lotes,
                     LotesAsignados = batches,
@@ -467,7 +470,7 @@ namespace Omicron.SapAdapter.Services.Sap
                     SysNumber = x.SysNumber,
                     NumeroLote = x.DistNumber,
                     CantidadAsignada = x.CommitQty,
-                    CantidadDisponible = x.Quantity - x.CommitQty,
+                    CantidadDisponible = Math.Round(x.Quantity - x.CommitQty, 6),
                 });
             });
 
