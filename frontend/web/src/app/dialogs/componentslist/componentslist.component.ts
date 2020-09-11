a6b1b3c47df27dd66b75f5e9df246bda65599cdf
gaboxlive <gabriel.cruz@axity.com>
2020-09-11 16:54:10 -0500

    Actualizacion de mi lista

--- frontend/web/src/app/dialogs/componentslist/componentslist.component.ts
+++ frontend/web/src/app/dialogs/componentslist/componentslist.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, Inject, ViewChild, AfterViewInit } from '@angular/core';
 
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
-import { IMyNewListReq, BaseComponent, IMyCustomListRes } from 'src/app/model/http/listacomponentes';
+import { BaseComponent } from 'src/app/model/http/listacomponentes';
 import { DataService } from 'src/app/services/data.service';
-import { FormControl, Validators } from '@angular/forms';
 import { OrdersService } from 'src/app/services/orders.service';
 import { Messages } from 'src/app/constants/messages';
 import { MatPaginator, MatTableDataSource} from '@angular/material';
@@ -13,8 +12,8 @@ import { MatPaginator, MatTableDataSource} from '@angular/material';
   templateUrl: './componentslist.component.html',
   styleUrls: ['./componentslist.component.scss']
 })
-export class ComponentslistComponent implements OnInit, AfterViewInit {
-  displayedColumns: string[] = ['codigo', 'nombre'];
+export class ComponentslistComponent implements AfterViewInit {
+  displayedColumns: string[] = ['nombre'];
   dataSource = new MatTableDataSource<BaseComponent>();
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   constructor(
@@ -24,12 +23,9 @@ export class ComponentslistComponent implements OnInit, AfterViewInit {
     private orderService: OrdersService
   ) { }
 
-  ngOnInit() {
-    this.getCustomList();
-  }
-
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.getCustomList();
   }
 
   getCustomList() {
@@ -39,4 +35,12 @@ export class ComponentslistComponent implements OnInit, AfterViewInit {
     });
   }
 
+  selectComponent(element: BaseComponent) {
+    this.dataService.presentToastCustom(Messages.confirmReplaceWithListComponents, 'question', '', true, true).then( (res: any) => {
+      if (res.isConfirmed) {
+        console.log('fila seleccionada: ', element.components)
+        this.dialogRef.close(element.components);
+      }
+    });
+  }
 }
