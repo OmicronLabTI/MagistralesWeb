e9fd6399005792361bc140fa30b314273e4b3e53
gaboxlive <gabriel.cruz@axity.com>
2020-09-11 14:57:42 -0500

    Actualizacion

--- frontend/web/src/app/dialogs/componentslist/componentslist.component.ts
+++ frontend/web/src/app/dialogs/componentslist/componentslist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild, AfterViewInit } from '@angular/core';
 
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import { IMyNewListReq, BaseComponent, IMyCustomListRes } from 'src/app/model/http/listacomponentes';
@@ -6,16 +6,17 @@ import { DataService } from 'src/app/services/data.service';
 import { FormControl, Validators } from '@angular/forms';
 import { OrdersService } from 'src/app/services/orders.service';
 import { Messages } from 'src/app/constants/messages';
-import { MatTableDataSource} from '@angular/material';
+import { MatPaginator, MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-componentslist',
   templateUrl: './componentslist.component.html',
   styleUrls: ['./componentslist.component.scss']
 })
-export class ComponentslistComponent implements OnInit {
+export class ComponentslistComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = ['codigo', 'nombre'];
   dataSource = new MatTableDataSource<BaseComponent>();
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   constructor(
     private dialogRef: MatDialogRef<ComponentslistComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
@@ -27,8 +28,13 @@ export class ComponentslistComponent implements OnInit {
     this.getCustomList();
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
   getCustomList() {
-    this.orderService.getCustomList(this.data.code).subscribe( result => {
+    console.log(this.data.code);
+    this.orderService.getCustomList('?productId=' + this.data.code).subscribe( result => {
       this.dataSource.data = result.response;
     });
   }
