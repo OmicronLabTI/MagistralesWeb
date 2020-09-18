31d9e153339f2edd87c0053b8376ca6384cccb7c
Vicente Cantú <vicente.cantu@axity.com>
2020-09-18 17:50:01 -0500

    Se agrego el logo de la empresa en todas las vistas en la esquina superior derecha. task: OMICAPPIOS-597

--- frontend/ios/Omicron/Screens/Lots/LotsViewController.swift
+++ frontend/ios/Omicron/Screens/Lots/LotsViewController.swift
@@ -235,7 +235,7 @@ class LotsViewController: UIViewController {
             }
             
             let commentsIcons = UIBarButtonItem(image: UIImage(systemName: iconName), style: .plain, target: self, action: #selector(self.goToCommentsViewController))
-            self.navigationItem.rightBarButtonItem = commentsIcons
+            self.navigationItem.rightBarButtonItems = [getOmniconLogo(), commentsIcons]
         
         }
     }
