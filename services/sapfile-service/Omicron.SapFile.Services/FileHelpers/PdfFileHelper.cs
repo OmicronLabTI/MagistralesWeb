577ee5e9b700381debc59b51404020081fb81a1a
HeribertoMT <>
2020-09-23 10:19:17 -0500

    Add iText Sharp package

---
+++ services/sapfile-service/Omicron.SapFile.Services/FileHelpers/PdfFileHelper.cs
@@ -0,0 +1,16 @@
+﻿// <summary>
+// <copyright file="PdfFileHelper.cs" company="Axity">
+// This source code is Copyright Axity and MAY NOT be copied, reproduced,
+// published, distributed or transmitted to or stored in any manner without prior
+// written consent from Axity (www.axity.com).
+// </copyright>
+// </summary>
+namespace Omicron.SapFile.Services.FileHelpers
+{
+    /// <summary>
+    /// Helper for pdf files
+    /// </summary>
+    public class PdfFileHelper
+    {
+    }
+}
