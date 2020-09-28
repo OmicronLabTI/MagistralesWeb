74a401239d9b363819782e2b558bc29e41308657
HeribertoMT <>
2020-09-28 17:09:05 -0500

    Fix tests

--- frontend/web/src/app/pages/material-request/material-request.component.spec.ts
+++ frontend/web/src/app/pages/material-request/material-request.component.spec.ts
@@ -18,8 +18,4 @@ describe('MaterialRequestComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
