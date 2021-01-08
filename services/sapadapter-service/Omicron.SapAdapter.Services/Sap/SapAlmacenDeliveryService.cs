c52cd1d8de7a77ee427f033bf0cf173793b49bb7
GustavoRamirez <gusiz@hotmail.com>
2021-01-08 17:21:23 -0600

    Se agrega que busque pedidos par los comentarios

--- services/sapadapter-service/Omicron.SapAdapter.Services/Sap/SapAlmacenDeliveryService.cs
+++ services/sapadapter-service/Omicron.SapAdapter.Services/Sap/SapAlmacenDeliveryService.cs
@@ -128,7 +128,7 @@ namespace Omicron.SapAdapter.Services.Sap
                 var header = headers.FirstOrDefault(x => x.DocNum == d);
                 var deliveryDetail = details.Where(x => x.DeliveryId == d).ToList();
                 var saleOrder = deliveryDetail.FirstOrDefault() != null ? deliveryDetail.FirstOrDefault().BaseEntry : 0;
-                var userOrder = userOrders.FirstOrDefault(x => x.Salesorderid == saleOrder.ToString());
+                var userOrder = userOrders.FirstOrDefault(x => string.IsNullOrEmpty(x.Productionorderid) && x.Salesorderid == saleOrder.ToString());
                 var userOrdersBySale = userOrders.Where(x => x.Salesorderid == saleOrder.ToString()).ToList();
                 var lineProductsBySale = lineProducts.Where(x => x.SaleOrderId == saleOrder).ToList();
 
