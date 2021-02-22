62d8bdf6f8d9e8fa9b6140f35046099898ee1f1e
Sergio Flores Ramírez <sergio.flores@axity.com>
2021-02-22 12:56:22 -0600

    Se agrega extension del controller para obtener menos de 200 lineas de código

--- frontend/ios/Omicron/Screens/OrderDetailForm/OrderDetailFormViewController.swift
+++ frontend/ios/Omicron/Screens/OrderDetailForm/OrderDetailFormViewController.swift
@@ -13,10 +13,12 @@ import RxCocoa
 import Resolver
 
 class OrderDetailFormViewController: FormViewController {
+
     // MARK: Variables
     @Injected var orderDetailViewModel: OrderDetailViewModel
     @Injected var orderDetailFormViewModel: OrderDetailFormViewModel
     @Injected var lottieManager: LottieManager
+
     weak var dataOfTable: OrderDetail?
     var indexOfItemSelected: Int = -1
     var disposeBag = DisposeBag()
@@ -24,15 +26,13 @@ class OrderDetailFormViewController: FormViewController {
     weak var requiredQuantity: TextRow?
     weak var werehouse: PickerInlineRow<String>?
     let formatter = UtilsManager.shared.formatterDoublesTo6Decimals()
+
     // MARK: Life cycles
     override func viewDidLoad() {
         super.viewDidLoad()
         self.buildForm()
         self.viewModelBinding()
         self.isModalInPresentation = true
-    }
-    deinit {
-        print("Se muere el ipad")
     }
     // MARK: Functions
     // swiftlint:disable function_body_length
@@ -84,7 +84,6 @@ class OrderDetailFormViewController: FormViewController {
                         self?.assigResultToRequireQty(result: result)
                     }
                 }
-                // Validaciones
                 var rules = RuleSet<String>()
                 rules.add(rule: fieldNoEmpty)
                 rules.add(rule: fieldShouldNotNegativeNumbers)
@@ -137,7 +136,6 @@ class OrderDetailFormViewController: FormViewController {
                         self?.assingResultToBaseQtyField(result: result)
                     }
                 }
-                // Validaciones
                 var rules = RuleSet<String>()
                 rules.add(rule: fieldNoEmpty)
                 rules.add(rule: fieldShouldNotNegativeNumbers)
@@ -217,66 +215,5 @@ class OrderDetailFormViewController: FormViewController {
             .onCellSelection { [weak self] _, _ in
                 self?.dismiss(animated: true)
         }
-    }
-    func saveChanges () {
-        if self.canOperation(rowValue: self.baseQuantity?.value ?? "f")
-            && self.canOperation(rowValue: (self.requiredQuantity?.value) ?? "r") {
-            let requiredQuantityValue = Double( self.requiredQuantity?.value ?? "0.0")
-            let baseQuantityValue = Double(self.baseQuantity?.value ?? "0.0")
-            guard let dataOfTable = self.dataOfTable else { return }
-            self.orderDetailFormViewModel.editItemTable(index: self.indexOfItemSelected,
-                                                        data: dataOfTable, baseQuantity: baseQuantityValue ?? 0.0,
-                                                        requiredQuantity: requiredQuantityValue ?? 0.0,
-                                                        werehouse: werehouse?.value ?? CommonStrings.empty)
-        }
-    }
-    func viewModelBinding () {
-        // Muestra o oculta el loading
-        orderDetailFormViewModel.loading.observeOn(MainScheduler.instance)
-            .subscribe(onNext: { [weak self] showLoading in
-            if showLoading {
-                self?.lottieManager.showLoading()
-                return
-            }
-            self?.lottieManager.hideLoading()
-        }).disposed(by: self.disposeBag)
-        orderDetailFormViewModel.showAlert.observeOn(MainScheduler.instance).subscribe(onNext: { [weak self] message in
-            let alert = UIAlertController(title: message, message: nil, preferredStyle: .alert)
-            let okAction = UIAlertAction(title: CommonStrings.OKConst, style: .default,
-                                         handler: {[weak self] _ in self?.refreshOrderDetail()})
-            alert.addAction(okAction)
-            self?.present(alert, animated: true, completion: nil)
-        }).disposed(by: self.disposeBag)
-        // Aqui es en donde se hace el manda a llamar el servicio para volver a traer
-        // los datos de detalle de la fórmnula
-        orderDetailFormViewModel.success.observeOn(MainScheduler.instance).subscribe(onNext: { [weak self] _ in
-            self?.orderDetailViewModel.getOrdenDetail()
-        }).disposed(by: self.disposeBag)
-    }
-    func refreshOrderDetail() {
-        self.dismiss(animated: true)
-    }
-    func canOperation(rowValue: String) -> Bool {
-        let range = NSRange(location: 0, length: rowValue.description.utf16.count)
-        let regex = try? NSRegularExpression(pattern: "^([0-9]+)?(\\.([0-9]{1,6})?)?$")
-        return regex?.firstMatch(in: rowValue.description, options: [], range: range) != nil
-    }
-
-    func assingResultToBaseQtyField(result: Decimal) {
-        let baseQuantityField = self.form.rowBy(tag: "baseQuantity") as? TextRow
-        baseQuantityField?.value =
-            self.dataOfTable?.details?[self.indexOfItemSelected].unit == "Pieza" ?
-                String(format: "%.0f", NSDecimalNumber(decimal: result).doubleValue) :
-            String(format: "%.6f", NSDecimalNumber(decimal: result).doubleValue)
-        baseQuantityField?.reload()
-    }
-
-    func assigResultToRequireQty(result: Decimal) {
-        let requireQuantityField = self.form.rowBy(tag: "requiredQuantity") as? TextRow
-        requireQuantityField?.value =
-            self.dataOfTable?.details?[self.indexOfItemSelected].unit == "Pieza" ?
-            String(format: "%.0f", NSDecimalNumber(decimal: result).doubleValue) :
-            String(format: "%.6f", NSDecimalNumber(decimal: result).doubleValue)
-        requireQuantityField?.reload()
     }
 }
