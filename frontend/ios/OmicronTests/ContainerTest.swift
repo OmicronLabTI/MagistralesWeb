a761b5855887e0f656e79c669a71337b9c8dc31e
Vicente Cantu <vicente.cantu@axity.com>
2021-03-02 10:48:36 -0600

    se agrego ContainerTest.

---
+++ frontend/ios/OmicronTests/ContainerTest.swift
@@ -0,0 +1,42 @@
+//
+//  ContainerTest.swift
+//  OmicronTests
+//
+//  Created by Vicente Cantu Garcia on 02/03/21.
+//  Copyright © 2021 Diego Cárcamo. All rights reserved.
+//
+
+import XCTest
+import RxSwift
+import Resolver
+
+@testable import OmicronLab
+
+class ContainerTest: XCTestCase {
+
+    var containerViewModel: ContainerViewModel?
+    var disposeBag: DisposeBag?
+    @Injected var networkManager: NetworkManager
+
+    override func setUpWithError() throws {
+        containerViewModel = ContainerViewModel()
+        disposeBag = DisposeBag()
+    }
+
+    override func tearDownWithError() throws {
+        containerViewModel = nil
+        disposeBag = nil
+    }
+    
+    func testGetContainerData() {
+        containerViewModel!.getContainerData()
+        guard let userData = Persistence.shared.getUserData(), let userId = userData.id else { return }
+        networkManager
+            .getContainer(userId: userId)
+            .subscribe(onNext: { containerResponse in
+                XCTAssertNotNil(containerResponse)
+            })
+            .disposed(by: disposeBag!)
+    }
+
+}
