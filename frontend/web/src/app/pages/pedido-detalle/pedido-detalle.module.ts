8294b0e398a0f859c8a9ceadb32b201fc87f8352
benny alva <bennytescha@gmail.com>
2021-03-05 14:54:48 -0600

    resolved conflics on all pages and services

--- frontend/web/src/app/pages/pedido-detalle/pedido-detalle.module.ts
+++ frontend/web/src/app/pages/pedido-detalle/pedido-detalle.module.ts
@@ -5,12 +5,11 @@ import { PedidoDetalleRoutingModule } from './pedido-detalle-routing.module';
 import {PedidoDetalleComponent} from './pedido-detalle.component';
 import {MATERIAL_COMPONENTS} from '../../app.material';
 import {FormsModule} from '@angular/forms';
-import {AddCommentsDialogComponent} from '../../dialogs/add-comments-dialog/add-comments-dialog.component';
 import {ComponentsModule} from '../../components/components.module';
 
 
 @NgModule({
-  declarations: [PedidoDetalleComponent, AddCommentsDialogComponent],
+  declarations: [PedidoDetalleComponent],
     imports: [
         CommonModule,
         PedidoDetalleRoutingModule,
@@ -19,6 +18,6 @@ import {ComponentsModule} from '../../components/components.module';
         ComponentsModule
     ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    entryComponents: [AddCommentsDialogComponent]
+    entryComponents: []
 })
 export class PedidoDetalleModule { }
