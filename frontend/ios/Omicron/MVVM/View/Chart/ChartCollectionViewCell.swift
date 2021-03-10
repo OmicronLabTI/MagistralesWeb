9af0e07155644402752518fc4f7d51196c4e38c7
Vicente Cantu <vicente.cantu@axity.com>
2021-03-10 14:09:23 -0600

    se cambio el icono de las flechas.

--- frontend/ios/Omicron/MVVM/View/Chart/ChartCollectionViewCell.swift
+++ frontend/ios/Omicron/MVVM/View/Chart/ChartCollectionViewCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Charts
 
-class ChartCollectionViewCell: UICollectionViewCell {
+class ChartCollectionViewCell: UICollectionViewCell, ChartViewDelegate {
 
     @IBOutlet weak var pieChartView: PieChartView!
 
@@ -18,6 +18,7 @@ class ChartCollectionViewCell: UICollectionViewCell {
     }
 
     func setData(data: Workload?) {
+        pieChartView.delegate = self
         guard let data = data else { return }
         setDataToChart(data)
     }
@@ -69,6 +70,7 @@ class ChartCollectionViewCell: UICollectionViewCell {
         let set = PieChartDataSet(entries: entries, label: "")
         set.colors = colors
         set.sliceSpace = 2
+//        set.yValuePosition = .outsideSlice
 
         let data = PieChartData(dataSet: set)
         data.setValueFont(.systemFont(ofSize: 30, weight: .bold))
@@ -96,6 +98,41 @@ class ChartCollectionViewCell: UICollectionViewCell {
         pieChartView.transparentCircleRadiusPercent = 0.61
         pieChartView.drawCenterTextEnabled = true
 
+        let marker = ChartMarker()
+        marker.chartView = pieChartView
+        pieChartView.marker = marker
+
+    }
+    
+}
+
+class ChartMarker: MarkerView {
+    var text = ""
+
+    override func refreshContent(entry: ChartDataEntry, highlight: Highlight) {
+        super.refreshContent(entry: entry, highlight: highlight)
+        text = String(entry.y)
     }
 
+    override func draw(context: CGContext, point: CGPoint) {
+        super.draw(context: context, point: point)
+
+        var drawAttributes = [NSAttributedString.Key : Any]()
+        drawAttributes[.font] = UIFont.systemFont(ofSize: 20)
+        drawAttributes[.foregroundColor] = UIColor.white
+        drawAttributes[.backgroundColor] = UIColor.darkGray
+
+        self.bounds.size = (" \(text) " as NSString).size(withAttributes: drawAttributes)
+        self.offset = CGPoint(x: 0, y: -self.bounds.size.height - 2)
+
+        let offset = self.offsetForDrawing(atPoint: point)
+
+        drawText(text: " \(text) " as NSString, rect: CGRect(origin: CGPoint(x: point.x + offset.x, y: point.y + offset.y), size: self.bounds.size), withAttributes: drawAttributes)
+    }
+
+    func drawText(text: NSString, rect: CGRect, withAttributes attributes: [NSAttributedString.Key : Any]? = nil) {
+        let size = text.size(withAttributes: attributes)
+        let centeredRect = CGRect(x: rect.origin.x + (rect.size.width - size.width) / 2.0, y: rect.origin.y + (rect.size.height - size.height) / 2.0, width: size.width, height: size.height)
+        text.draw(in: centeredRect, withAttributes: attributes)
+    }
 }
