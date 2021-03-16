087c5e00f9e6208343a23a99e0dfba7404beb0ae
Elías D Sánchez <69357302+develiasdaniel@users.noreply.github.com>
2021-03-16 15:20:36 -0600

    add unit testing

--- services/reporting-service/Omicron.Reporting.Test/Omicron.Reporting.Test/Facade/Reporting/ReportingFacadeTest.cs
+++ services/reporting-service/Omicron.Reporting.Test/Omicron.Reporting.Test/Facade/Reporting/ReportingFacadeTest.cs
@@ -57,6 +57,10 @@ namespace Omicron.Reporting.Test.Facade.Request
                 .Setup(m => m.SendEmailLocalPackage(It.IsAny<SendLocalPackageModel>()))
                 .Returns(Task.FromResult(resultModel));
 
+            mockReportingService
+                .Setup(m => m.SendEmailCancelDeliveryOrders(It.IsAny<SendCancelDeliveryModel>()))
+                .Returns(Task.FromResult(resultModel));
+
             this.reportingFacade = new ReportingFacade(mockReportingService.Object, mapper);
         }
 
@@ -151,6 +155,24 @@ namespace Omicron.Reporting.Test.Facade.Request
 
             // arrange
             Assert.IsNotNull(response);
+        }
+
+        /// <summary>
+        /// Test facade map result.
+        /// </summary>
+        /// <returns>Nothing.</returns>
+        [Test]
+        public async Task SendEmailCancelDeliveryOrders()
+        {
+            // arrange
+            var requests = new SendCancelDeliveryDto();
+
+            // act
+            var response = await this.reportingFacade.SendEmailCancelDeliveryOrders(requests);
+
+            // arrange
+            Assert.IsNotNull(response);
+            Assert.IsTrue(response.Success);
         }
     }
 }
