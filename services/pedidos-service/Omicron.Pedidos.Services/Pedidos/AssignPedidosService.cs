b07e6d69521484ff42757e3d182abbee3451da03
Elías D Sánchez <69357302+develiasdaniel@users.noreply.github.com>
2021-03-21 14:56:03 -0600

    añadir sales log a asignar/automatico

--- services/pedidos-service/Omicron.Pedidos.Services/Pedidos/AssignPedidosService.cs
+++ services/pedidos-service/Omicron.Pedidos.Services/Pedidos/AssignPedidosService.cs
@@ -124,6 +124,10 @@ namespace Omicron.Pedidos.Services.Pedidos
                     var ordenType = string.IsNullOrEmpty(x.Productionorderid) ? ServiceConstants.OrdenVenta : ServiceConstants.OrdenFab;
                     var textAction = string.IsNullOrEmpty(x.Productionorderid) ? string.Format(ServiceConstants.AsignarVenta, userSaleOrder.Item1[saleOrderInt]) : string.Format(ServiceConstants.AsignarOrden, userSaleOrder.Item1[saleOrderInt]);
                     listOrderToInsert.AddRange(ServiceUtils.CreateOrderLog(assignModel.UserLogistic, new List<int> { orderId }, textAction, ordenType));
+                    if (previousStatus != x.Status)
+                    {
+                        listOrderLogToInsert.AddRange(ServiceUtils.AddSalesLog(assignModel.UserLogistic, "name", new List<UserOrderModel> { x }));
+                    }
                 }
             });
 
