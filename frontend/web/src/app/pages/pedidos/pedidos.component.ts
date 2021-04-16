3396401f38288444fa35446ec097252b0b312664
benny alva <bennytescha@gmail.com>
2021-04-16 13:44:37 -0500

    new validation on pedidos

--- frontend/web/src/app/pages/pedidos/pedidos.component.ts
+++ frontend/web/src/app/pages/pedidos/pedidos.component.ts
@@ -123,6 +123,8 @@ export class PedidosComponent implements OnInit, OnDestroy {
         this.lengthPaginator = pedidoRes.comments;
         this.dataSource.data = pedidoRes.response;
         this.dataSource.data.forEach(element => {
+              element.pedidoStatus = element.pedidoStatus === ConstStatus.initial && element.canceled === ConstStatus.canceled ?
+                  ConstStatus.cancelado : element.pedidoStatus;
               switch (element.pedidoStatus) {
                   case ConstStatus.abierto:
                       element.class = 'abierto';
