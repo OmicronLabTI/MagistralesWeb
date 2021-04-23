4dc1922be3fff52f35a15e135d27ca93ca807a6f
ErikaRosas <40810601+ErikaRosas@users.noreply.github.com>
2021-04-23 17:28:46 -0500

    Corregir codigo smell de reporting

--- services/reporting-service/Omicron.Reporting.DependencyInjection/DependencyInjector.cs
+++ services/reporting-service/Omicron.Reporting.DependencyInjection/DependencyInjector.cs
@@ -9,14 +9,14 @@
 namespace Omicron.Reporting.DependencyInjection
 {
     using AutoMapper;
-    using Omicron.Reporting.Entities.Context;
-    using Omicron.Reporting.Services.Mapping;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using Omicron.Reporting.Entities.Context;
     using Omicron.Reporting.Facade.Request;
     using Omicron.Reporting.Services;
     using Omicron.Reporting.Services.Clients;
+    using Omicron.Reporting.Services.Mapping;
 
     /// <summary>
     /// Class for DependencyInjector.
