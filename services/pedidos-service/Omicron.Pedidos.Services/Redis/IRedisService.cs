002a72f2c071fc78d6406def1689a939d82bdb57
Gustavo Ramirez <gustavo.ramirez@axity.com>
2021-05-18 17:40:14 -0500

    Se agrega redis para el bloqueo de planificar

--- services/pedidos-service/Omicron.Pedidos.Services/Redis/IRedisService.cs
+++ services/pedidos-service/Omicron.Pedidos.Services/Redis/IRedisService.cs
@@ -30,5 +30,21 @@ namespace Omicron.Pedidos.Services.Redis
         /// <param name="value">the value.</param>
         /// <returns>the data.</returns>
         Task<bool> WriteToRedis(string key, string value);
+
+        /// <summary>
+        /// Writes to Reddis.
+        /// </summary>
+        /// <param name="key">the key.</param>
+        /// <param name="value">the value.</param>
+        /// <param name="timeToLive">Time to live.</param>
+        /// <returns>the data.</returns>
+        Task<bool> WriteToRedis(string key, string value, TimeSpan timeToLive);
+
+        /// <summary>
+        /// Deletes a key from redis.
+        /// </summary>
+        /// <param name="key">the key.</param>
+        /// <returns>the data.</returns>
+        Task<bool> DeleteKey(string key);
     }
 }
