3818441dc3665bacba291de003cfd53862951afd
Gustavo Ramirez <gustavo.ramirez@axity.com>
2021-06-29 17:37:22 -0500

    Se modifican arhivos

---
+++ frontend/web/src/environments/environment.uat.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    baseUrl: 'http://201.163.179.28:8000', // qa
+    apiLogin: '/api/oauth/oauthrs',
+    apiCatalogs: '/api/catalogos',
+    apiUsers: '/api/usuarios',
+  };
