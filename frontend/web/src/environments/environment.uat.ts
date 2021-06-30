25976ec5a7fba8ba5f8525932979040bc196c796
Gustavo Ramirez <gustavo.ramirez@axity.com>
2021-06-30 18:05:31 -0500

    Se cambia archivo

--- frontend/web/src/environments/environment.uat.ts
+++ frontend/web/src/environments/environment.uat.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://omicronapiuat.eastus2.cloudapp.azure.com', // qa
+    baseUrl: 'https://omicronapiuat.eastus2.cloudapp.azure.com', // qa
     apiLogin: '/api/oauth/oauthrs',
     apiCatalogs: '/api/catalogos',
     apiUsers: '/api/usuarios',
