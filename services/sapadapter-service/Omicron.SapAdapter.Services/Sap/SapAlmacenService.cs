ce9f92513957157585135321de9661c56829b774
Gustavo Ramirez <gustavo.ramirez@axity.com>
2021-12-10 11:47:17 -0600

    Para por recibir se quitan back orders

--- services/sapadapter-service/Omicron.SapAdapter.Services/Sap/SapAlmacenService.cs
+++ services/sapadapter-service/Omicron.SapAdapter.Services/Sap/SapAlmacenService.cs
@@ -369,7 +369,7 @@ namespace Omicron.SapAdapter.Services.Sap
                 allIds.AddRange(lineProducts.Where(x => string.IsNullOrEmpty(x.ItemCode)).Select(y => y.SaleOrderId));
 
                 var idsToLook = userModels.Where(x => string.IsNullOrEmpty(x.Productionorderid) && x.Status == ServiceConstants.Finalizado && !ServiceConstants.StatusToIgnorePorRecibir.Contains(x.StatusAlmacen)).Select(y => int.Parse(y.Salesorderid)).ToList();
-                idsToLook.AddRange(lineProducts.Where(x => string.IsNullOrEmpty(x.ItemCode) && x.StatusAlmacen != ServiceConstants.Almacenado).Select(y => y.SaleOrderId));
+                idsToLook.AddRange(lineProducts.Where(x => string.IsNullOrEmpty(x.ItemCode) && !ServiceConstants.StatusToIgnorePorRecibir.Contains(x.StatusAlmacen)).Select(y => y.SaleOrderId));
                 idsToLook.AddRange(sapOrders.Where(x => !allIds.Contains(x.DocNum)).Select(y => y.DocNum));
                 listToReturn.AddRange(sapOrders.Where(x => idsToLook.Contains(x.DocNum)));
             }
