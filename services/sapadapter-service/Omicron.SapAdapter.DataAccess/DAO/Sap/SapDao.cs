dac2c6d831c0199841a0701b3fee69a39943a52b
Elías D Sánchez <69357302+develiasdaniel@users.noreply.github.com>
2021-12-13 13:11:41 -0600

    OM-913 bug fix
    agregar status si el pedido esta en status cancelado (sap) en busqueda avanzada recepcion pedido

--- services/sapadapter-service/Omicron.SapAdapter.DataAccess/DAO/Sap/SapDao.cs
+++ services/sapadapter-service/Omicron.SapAdapter.DataAccess/DAO/Sap/SapDao.cs
@@ -1365,6 +1365,7 @@ namespace Omicron.SapAdapter.DataAccess.DAO.Sap
                              PedidoMuestra = order.PedidoMuestra,
                              Comments = order.Comments,
                              AsesorId = order.AsesorId,
+                             Canceled = order.Canceled,
                          });
 
             return await this.RetryQuery<CompleteOrderModel>(query);
