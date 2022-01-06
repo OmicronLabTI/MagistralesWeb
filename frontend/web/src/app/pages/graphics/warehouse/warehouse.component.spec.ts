36ca1dbe874a23eee31c633aab6ced1b1cf36519
Lisbeth Espinoza <lisbeth.espinoza@axity.com>
2022-01-06 10:31:32 -0600

    Adecuaciones pruebas unitarias

--- frontend/web/src/app/pages/graphics/warehouse/warehouse.component.spec.ts
+++ frontend/web/src/app/pages/graphics/warehouse/warehouse.component.spec.ts
@@ -7,25 +7,43 @@ import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {WarehouseMock} from '../../../../mocks/warehouseListMock';
 import {IncidentsService} from '../../../services/incidents.service';
 import {of} from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DataService } from 'src/app/services/data.service';
+import { ErrorService } from 'src/app/services/error.service';
 
-describe('WarehouseComponent', () => {
+fdescribe('WarehouseComponent', () => {
   let component: WarehouseComponent;
   let fixture: ComponentFixture<WarehouseComponent>;
+  let dataServiceSpy: jasmine.SpyObj<DataService>;
+  let errorServiceSpy;
   let incidentsServiceSpy;
 
   beforeEach(async(() => {
+    dataServiceSpy = jasmine.createSpyObj<DataService>('DataService', [
+      'setUrlActive',
+      'getPercentageByItem'
+    ]);
     incidentsServiceSpy = jasmine.createSpyObj<IncidentsService>('IncidentsService', [
       'getWarehouseGraph'
     ]);
+    errorServiceSpy = jasmine.createSpyObj<ErrorService>('ErrorService', [
+      'httpError'
+    ]);
+    dataServiceSpy.getPercentageByItem.and.callFake(() => {
+      return '5';
+    });
     incidentsServiceSpy.getWarehouseGraph.and.callFake(() => {
       return of(WarehouseMock);
     });
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule,
+      RouterTestingModule],
       declarations: [ WarehouseComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [DatePipe,
         { provide: IncidentsService, useValue: incidentsServiceSpy },
+        { provide: DataService, useValue: dataServiceSpy },
+        { provide: ErrorService, useValue: errorServiceSpy },
       ]
     })
     .compileComponents();
@@ -37,14 +55,15 @@ describe('WarehouseComponent', () => {
     fixture.detectChanges();
   });
 
-/*  it('should create', () => {
-    component.localPackages = WarehouseMock.response.filter(
-        itemGraph => itemGraph.graphType.toLowerCase() === GraphType.packageLocal.toLowerCase());
-    component.foreignPackages = WarehouseMock.response.filter(
-        itemGraph => itemGraph.graphType.toLowerCase() === GraphType.foreignPackage.toLowerCase());
+  it('should create', () => {
+    expect(component).toBeTruthy();
+    // component.localPackages = WarehouseMock.response.filter(
+    //     itemGraph => itemGraph.graphType.toLowerCase() === GraphType.packageLocal.toLowerCase());
+    // component.foreignPackages = WarehouseMock.response.filter(
+    //     itemGraph => itemGraph.graphType.toLowerCase() === GraphType.foreignPackage.toLowerCase());
 
-   /!* expect(component).toBeTruthy();*!/
-  });*/
+  //  /!* expect(component).toBeTruthy();*!/
+  });
  /* it('should generateDataWarehouse', () => {
     component.checkNewRange('20/02/21-22/05/21');
     /!*expect(component.itemsGraph.length > 0).toBeTruthy();*!/
