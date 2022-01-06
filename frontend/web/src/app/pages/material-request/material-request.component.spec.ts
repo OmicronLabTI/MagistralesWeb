25239e94661ae0c8a2df8f352032b2a0fb3d0032
Sergio Flores Ramírez <sergio.flores@axity.com>
2022-01-06 15:14:10 -0600

    TITLE: SE QUITA F EN LA PRUEBAS DEL COMPONENTE

--- frontend/web/src/app/pages/material-request/material-request.component.spec.ts
+++ frontend/web/src/app/pages/material-request/material-request.component.spec.ts
@@ -20,7 +20,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material';
 
-fdescribe('MaterialRequestComponent', () => {
+describe('MaterialRequestComponent', () => {
   let component: MaterialRequestComponent;
   let fixture: ComponentFixture<MaterialRequestComponent>;
 
