f4cb9793c98851e13940dd98557cd7343a45822b
Sergio Flores Ramírez <sergio.flores@axity.com>
2022-01-10 14:17:00 -0600

    TITLE: USER-LIST COMPONENT COVERAGE 94%

--- frontend/web/src/app/pages/user-list/user-list.component.spec.ts
+++ frontend/web/src/app/pages/user-list/user-list.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserListRoutingModule } from './user-list-routing.module';
@@ -10,7 +10,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -27,17 +27,27 @@ import { PageEvent } from '@angular/material/paginator';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ObservableService } from '../../services/observable.service';
 import { MessagesService } from 'src/app/services/messages.service';
+export class MatDialogMock {
+  open() {
+    return {
+      afterClosed: () => of({}),
+    };
+  }
+}
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
   let messagesServiceSpy: jasmine.SpyObj<MessagesService>;
-  let userServiceSpy;
+  let userServiceSpy: jasmine.SpyObj<UsersService>;
   let dataServiceSpy;
   let errorServiceSpy;
   let observableServiceSpy: jasmine.SpyObj<ObservableService>;
 
+  let matDialog: MatDialogMock;
+
   beforeEach(async(() => {
+    matDialog = new MatDialogMock();
     userServiceSpy = jasmine.createSpyObj<UsersService>('UsersService',
       [
         'getUsers'
@@ -94,6 +104,7 @@ describe('UserListComponent', () => {
         { provide: ErrorService, useValue: errorServiceSpy },
         { provide: ObservableService, useValue: observableServiceSpy },
         { provide: MessagesService, useValue: messagesServiceSpy },
+        { provide: MatDialog, useValue: matDialog },
       ]
     })
       .compileComponents();
@@ -163,5 +174,35 @@ describe('UserListComponent', () => {
     expect(component.offset).toEqual(0);
     expect(component.limit).toEqual(10);
   });
-
+  it('getCallHttpService success', () => {
+    const callHttpService = HttpServiceTOCall.USERS;
+    observableServiceSpy.getCallHttpService.and.returnValue(of(callHttpService));
+    component.ngOnInit();
+    expect(observableServiceSpy.setMessageGeneralCallHttp).toHaveBeenCalled();
+    expect(userServiceSpy.getUsers).toHaveBeenCalled();
+  });
+  it('should open SearchUsers dialog', () => {
+    const data = {
+      userNameSe: 'Juan',
+      firstNameSe: 'Perez',
+      lastNameSe: 'Perez',
+      userTypeRSe: '1',
+      activoSe: '1',
+      asignableSe: '1',
+      classificationQFBSe: 'BQ',
+    };
+    spyOn(matDialog, 'open').and.returnValue({
+      afterClosed: () => of(data),
+    } as MatDialogRef<typeof component>);
+    component.openSearchUsers();
+    expect(matDialog.open).toHaveBeenCalled();
+    expect(component.fullQueryString).toBe('status=1&assignable=1&fname=Perez&lname=Perez&user=Juan&role=1&typeQfb=BQ');
+  });
+  it('should open dialog AddUserDialogComponent', () => {
+    spyOn(matDialog, 'open').and.returnValue({
+      afterClosed: () => of({}),
+    } as MatDialogRef<typeof component>);
+    component.openDialog('', 'da423-sdf34-23');
+    expect(matDialog.open).toHaveBeenCalled();
+  });
 });
