402745e6333e8551b679a32edcf40adda2c853d9
Sergio Flores Ramírez <sergio.flores@axity.com>
2022-01-11 12:45:30 -0600

    PRUEBAS AUN NO TERMINADAS DATASERVICE

--- frontend/web/src/app/services/data.service.spec.ts
+++ frontend/web/src/app/services/data.service.spec.ts
@@ -1,8 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 import { DatePipe } from '@angular/common';
 import { DataService } from './data.service';
-import { Catalogs } from '../model/http/pedidos';
+import { Catalogs, ParamsPedidos } from '../model/http/pedidos';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ConstOrders, TypeToSeeTap } from '../constants/const';
 
 describe('DataService', () => {
   const catalogs = new Catalogs();
@@ -27,5 +28,23 @@ describe('DataService', () => {
     expect(service.getIsToSaveAnything()).toBeFalsy();
     service.setIsToSaveAnything(true);
     expect(service.getIsToSaveAnything()).toBeTruthy();
+  });
+  it('should getfiniOrffin', () => {
+    const service: DataService = TestBed.get(DataService);
+    const paramPedidos = new ParamsPedidos();
+    const result = service.getfiniOrffin(paramPedidos, '12/12/21');
+    expect(result).toEqual('?ffin=12/12/21');
+  });
+  it('should getfiniOrffin with defaultDateInit', () => {
+    const service: DataService = TestBed.get(DataService);
+    const paramPedidos = new ParamsPedidos();
+    paramPedidos.dateType = ConstOrders.defaultDateInit;
+    const result = service.getfiniOrffin(paramPedidos, '12/12/21');
+    expect(result).toEqual('?fini=12/12/21');
+  });
+  it('should getFormattedNumber', () => {
+    const service: DataService = TestBed.get(DataService);
+    const result = service.getFormattedNumber(5);
+    expect(result).toEqual('5');
   });
 });
