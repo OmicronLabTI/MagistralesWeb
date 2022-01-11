3951291ea89402d00f653ea6881826e4bd857191
Sergio Flores Ramírez <sergio.flores@axity.com>
2022-01-11 10:56:26 -0600

    Pruebas

--- frontend/web/src/app/services/consume.service.spec.ts
+++ frontend/web/src/app/services/consume.service.spec.ts
@@ -1,18 +1,18 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { ConsumeService } from './consume.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { Observable } from 'rxjs';
 import { DatePipe } from '@angular/common';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ObservableService } from './observable.service';
-describe('ConsumeService', () => {
+
+fdescribe('ConsumeService', () => {
+  const ENDPOINT = 'http://google.com';
   beforeEach(() => TestBed.configureTestingModule({
     imports: [
       HttpClientTestingModule, RouterTestingModule
     ],
     providers: [
-      ObservableService,
       DatePipe
     ]
   }));
@@ -24,24 +24,37 @@ describe('ConsumeService', () => {
 
   it('should create get observable', () => {
     const service: ConsumeService = TestBed.get(ConsumeService);
-    const obs = service.httpGet<any>('http://google.com');
+    const obs = service.httpGet<any>(ENDPOINT);
     expect(obs instanceof Observable).toBeTruthy();
   });
 
   it('should create post observable', () => {
     const service: ConsumeService = TestBed.get(ConsumeService);
-    const obs = service.httpPost<any>('http://google.com', {});
+    const obs = service.httpPost<any>(ENDPOINT, {});
     expect(obs instanceof Observable).toBeTruthy();
   });
 
   it('should create put observable', () => {
     const service: ConsumeService = TestBed.get(ConsumeService);
-    const obs = service.httpPut<any>('http://google.com', {});
+    const obs = service.httpPut<any>(ENDPOINT, {});
     expect(obs instanceof Observable).toBeTruthy();
   });
   it('should create patch observable', () => {
     const service: ConsumeService = TestBed.get(ConsumeService);
-    const obs = service.httpPatch<any>('http://google.com', {});
+    const obs = service.httpPatch<any>(ENDPOINT, {});
     expect(obs instanceof Observable).toBeTruthy();
   });
+  it('should create get observable', inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    const service: ConsumeService = TestBed.get(ConsumeService);
+    const obs = service.httpGet<any>(ENDPOINT, { param: 1 }, { header: 1 });
+    expect(obs instanceof Observable).toBeTruthy();
+    obs.subscribe(res => {
+      expect(res).toBeDefined();
+    });
+
+    const req = httpMock.expectOne(ENDPOINT);
+    expect(req.request.method).toEqual('GET');
+
+    req.flush({});
+  }));
 });
