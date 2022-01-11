d849eb5583271537cb4b1233673062fc4e792b64
Sergio Flores Ramírez <sergio.flores@axity.com>
2022-01-11 12:13:56 -0600

    TITLE: CONSUME SERVICE COVERAGE 100%

--- frontend/web/src/app/services/consume.service.spec.ts
+++ frontend/web/src/app/services/consume.service.spec.ts
@@ -1,13 +1,14 @@
-import { inject, TestBed } from '@angular/core/testing';
+import { fakeAsync, inject, TestBed, tick } from '@angular/core/testing';
 
 import { ConsumeService } from './consume.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { Observable } from 'rxjs';
 import { DatePipe } from '@angular/common';
 import { RouterTestingModule } from '@angular/router/testing';
+import { environment } from 'src/environments/environment';
 
-fdescribe('ConsumeService', () => {
-  const ENDPOINT = 'http://google.com';
+describe('ConsumeService', () => {
+  const ENDPOINT = environment.baseUrl;
   beforeEach(() => TestBed.configureTestingModule({
     imports: [
       HttpClientTestingModule, RouterTestingModule
@@ -22,39 +23,138 @@ fdescribe('ConsumeService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should create get observable', () => {
+  it('should create get observable', inject([HttpTestingController], fakeAsync((httpMock: HttpTestingController) => {
     const service: ConsumeService = TestBed.get(ConsumeService);
-    const obs = service.httpGet<any>(ENDPOINT);
-    expect(obs instanceof Observable).toBeTruthy();
-  });
-
-  it('should create post observable', () => {
-    const service: ConsumeService = TestBed.get(ConsumeService);
-    const obs = service.httpPost<any>(ENDPOINT, {});
-    expect(obs instanceof Observable).toBeTruthy();
-  });
-
-  it('should create put observable', () => {
-    const service: ConsumeService = TestBed.get(ConsumeService);
-    const obs = service.httpPut<any>(ENDPOINT, {});
-    expect(obs instanceof Observable).toBeTruthy();
-  });
-  it('should create patch observable', () => {
-    const service: ConsumeService = TestBed.get(ConsumeService);
-    const obs = service.httpPatch<any>(ENDPOINT, {});
-    expect(obs instanceof Observable).toBeTruthy();
-  });
-  it('should create get observable', inject([HttpTestingController], (httpMock: HttpTestingController) => {
-    const service: ConsumeService = TestBed.get(ConsumeService);
-    const obs = service.httpGet<any>(ENDPOINT, { param: 1 }, { header: 1 });
+    const obs = service.httpGet<any>(ENDPOINT, { params: 1 }, { header: 1 });
     expect(obs instanceof Observable).toBeTruthy();
     obs.subscribe(res => {
       expect(res).toBeDefined();
     });
-
-    const req = httpMock.expectOne(ENDPOINT);
-    expect(req.request.method).toEqual('GET');
-
+    const req = httpMock.expectOne({method: 'GET'});
     req.flush({});
+  })));
+
+  it('should create get observable with error', inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    const service: ConsumeService = TestBed.get(ConsumeService);
+    const obs = service.httpGet<any>(ENDPOINT, { param: 1 }, { header: 1 });
+    expect(obs instanceof Observable).toBeTruthy();
+    obs.subscribe(() => {}, error => {
+      expect(error).toBeDefined();
+    });
+
+    const req = httpMock.expectOne({method: 'GET'});
+    req.error(new ErrorEvent('err'));
+  }));
+
+  it('should create post observable', inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    const service: ConsumeService = TestBed.get(ConsumeService);
+    const obs = service.httpPost<any>(ENDPOINT, {}, { header: 1 });
+    expect(obs instanceof Observable).toBeTruthy();
+    obs.subscribe(res => {
+      expect(res).toBeDefined();
+    });
+    const req = httpMock.expectOne(ENDPOINT);
+    expect(req.request.method).toEqual('POST');
+    req.flush({});
+  }));
+  it('should create post observable with error', inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    const service: ConsumeService = TestBed.get(ConsumeService);
+    const obs = service.httpPost<any>(ENDPOINT, {}, { header: 1 });
+    expect(obs instanceof Observable).toBeTruthy();
+    obs.subscribe(() => {}, error => {
+      expect(error).toBeDefined();
+    });
+    const req = httpMock.expectOne(ENDPOINT);
+    expect(req.request.method).toEqual('POST');
+    req.error(new ErrorEvent('err'));
+  }));
+
+  it('should create put observable', inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    const service: ConsumeService = TestBed.get(ConsumeService);
+    const obs = service.httpPut<any>(ENDPOINT, { param: 1 }, { header: 1 });
+    expect(obs instanceof Observable).toBeTruthy();
+    obs.subscribe(res => {
+      expect(res).toBeDefined();
+    });
+    const req = httpMock.expectOne(ENDPOINT);
+    expect(req.request.method).toEqual('PUT');
+    req.flush({});
+  }));
+  it('should create put observable with error', inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    const service: ConsumeService = TestBed.get(ConsumeService);
+    const obs = service.httpPut<any>(ENDPOINT, { param: 1 }, { header: 1 });
+    expect(obs instanceof Observable).toBeTruthy();
+    obs.subscribe(() => {}, error => {
+      expect(error).toBeDefined();
+    });
+    const req = httpMock.expectOne(ENDPOINT);
+    expect(req.request.method).toEqual('PUT');
+    req.error(new ErrorEvent('err'));
+  }));
+  it('should create patch observable', inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    const service: ConsumeService = TestBed.get(ConsumeService);
+    const obs = service.httpPatch<any>(ENDPOINT, {},  { header: 1 });
+    expect(obs instanceof Observable).toBeTruthy();
+    obs.subscribe(res => {
+      expect(res).toBeDefined();
+    });
+    const req = httpMock.expectOne(ENDPOINT);
+    expect(req.request.method).toEqual('PATCH');
+    req.flush({});
+  }));
+  it('should create patch observable with error', inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    const service: ConsumeService = TestBed.get(ConsumeService);
+    const obs = service.httpPatch<any>(ENDPOINT, {},  { header: 1 });
+    expect(obs instanceof Observable).toBeTruthy();
+    obs.subscribe(() => {}, error => {
+      expect(error).toBeDefined();
+    });
+    const req = httpMock.expectOne(ENDPOINT);
+    expect(req.request.method).toEqual('PATCH');
+    req.error(new ErrorEvent('err'));
+  }));
+  it('should create httpDownloadFilePost observable', inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    const service: ConsumeService = TestBed.get(ConsumeService);
+    const obs = service.httpDownloadFilePost(ENDPOINT, {},  { header: 1 });
+    expect(obs instanceof Observable).toBeTruthy();
+    obs.subscribe(res => {
+      expect(res).toBeDefined();
+    });
+    const req = httpMock.expectOne(ENDPOINT);
+    expect(req.request.method).toEqual('POST');
+    req.flush(new Blob());
+  }));
+  it('should create httpDownloadFilePost observable with error', inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    const service: ConsumeService = TestBed.get(ConsumeService);
+    const obs = service.httpDownloadFilePost(ENDPOINT, {},  { header: 1 });
+    expect(obs instanceof Observable).toBeTruthy();
+    obs.subscribe(() => {}, error => {
+      expect(error).toBeDefined();
+    });
+    const req = httpMock.expectOne(ENDPOINT);
+    expect(req.request.method).toEqual('POST');
+    req.error(new ErrorEvent('err'));
+  }));
+  it('should create httpDelete observable', inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    const service: ConsumeService = TestBed.get(ConsumeService);
+    const obs = service.httpDelete(ENDPOINT);
+    expect(obs instanceof Observable).toBeTruthy();
+    obs.subscribe(res => {
+      expect(res).toBeDefined();
+    });
+    const req = httpMock.expectOne(ENDPOINT);
+    expect(req.request.method).toEqual('DELETE');
+    req.flush({});
+  }));
+  it('should create httpDelete observable', inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    const service: ConsumeService = TestBed.get(ConsumeService);
+    const obs = service.httpDelete(ENDPOINT);
+    expect(obs instanceof Observable).toBeTruthy();
+    obs.subscribe(() => {}, error => {
+      expect(error).toBeDefined();
+    });
+    const req = httpMock.expectOne(ENDPOINT);
+    expect(req.request.method).toEqual('DELETE');
+    req.error(new ErrorEvent('err'));
   }));
 });
