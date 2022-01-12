3b90e7efaf3efef10d96c471c8cdce4c6ac6d803
Elías D Sánchez <69357302+develiasdaniel@users.noreply.github.com>
2022-01-12 17:47:00 -0600

    OM-310 feature
    agregar correción y pruebas unitarias y agregar pruebas al servicio de azure

---
+++ services/reporting-service/Omicron.Reporting.Test/Omicron.Reporting.Test/Services/Reporting/AzureServiceTest.cs
@@ -0,0 +1,34 @@
+﻿// <summary>
+// <copyright file="AzureServiceTest.cs" company="Axity">
+// This source code is Copyright Axity and MAY NOT be copied, reproduced,
+// published, distributed or transmitted to or stored in any manner without prior
+// written consent from Axity (www.axity.com).
+// </copyright>
+// </summary>
+
+namespace Omicron.Reporting.Test.Services.Reporting
+{
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using Omicron.Reporting.Services.AzureServices;
+
+    /// <summary>
+    /// class for payment.
+    /// </summary>
+    public class AzureServiceTest
+    {
+        /// <summary>
+        /// Method to verify Get All ProcessPayments.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Test]
+        public async Task GetExistElementsFromAzure()
+        {
+            // act
+            var service = new AzureServices();
+            var result = await service.GetlementFromAzure(string.Empty, string.Empty, "https://omicronblobpruebas.blob.core.windows.net/resources/ClasificacionEnvases.xlsx");
+
+            Assert.IsNull(result);
+        }
+    }
+}
