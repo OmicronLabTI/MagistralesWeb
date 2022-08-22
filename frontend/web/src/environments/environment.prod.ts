6bc4cc50b28b7642fa7c9b57f0aba36501d6ad88
Gustavo Ramirez <gustavo.ramirez@axity.com>
2022-08-22 17:10:49 -0500

    Se regresa URL a magistralesProd

--- frontend/web/src/environments/environment.prod.ts
+++ frontend/web/src/environments/environment.prod.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: true,
-  baseUrl: 'https://omicron-api.eastus2.cloudapp.azure.com/',
-  //baseUrl: 'https://magistralesprod.omicronlab.com.mx',
+  baseUrl: 'https://magistralesprod.omicronlab.com.mx',
   apiLogin: '/api/oauth/oauthrs',
   apiCatalogs: '/api/catalogos',
   apiUsers: '/api/usuarios',
