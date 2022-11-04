c0324e85aa7fa5ce770954b487b988559c87250f
Gustavo Ramirez <gusiz@hotmail.com>
2022-11-04 12:24:44 -0600

    Se agrega propiedades para sobreescribir el total de un pedido

--- services/sapdiapi-service/Omicron.SapDiApi.Services/SapDiApi/SapCreateSaleOrder.cs
+++ services/sapdiapi-service/Omicron.SapDiApi.Services/SapDiApi/SapCreateSaleOrder.cs
@@ -83,6 +83,11 @@ namespace Omicron.SapDiApi.Services.SapDiApi
                 order.UserFields.Fields.Item("U_PedidoMuestra").Value = saleOrderModel.IsSample ? "Si" : "No";
                 order.UserFields.Fields.Item("U_Omigenomicstp").Value = saleOrderModel.IsOmigenomicsOrder ? "Y" : "N";
 
+                if (saleOrderModel.HasPromotionalCodeForOrder)
+                {
+                    order.DocTotal = saleOrderModel.TotalCost;
+                }
+
                 if (saleOrderModel.SlpCode != null)
                 {
                     order.SalesPersonCode = (int)saleOrderModel.SlpCode;
@@ -92,7 +97,6 @@ namespace Omicron.SapDiApi.Services.SapDiApi
                 {
                     order.DocumentsOwner = (int)saleOrderModel.EmployeeId;
                 }
-
 
                 if (!string.IsNullOrEmpty(attachment))
                 {
