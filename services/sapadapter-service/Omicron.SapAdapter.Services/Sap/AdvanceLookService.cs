e00dda65c15fbb2cac9f93dd0bd9b0ddb3089f8d
Alejandro Hernandez <jesus.hernandezm@axity.com>
2022-12-15 11:41:18 -0600

    usings

--- services/sapadapter-service/Omicron.SapAdapter.Services/Sap/AdvanceLookService.cs
+++ services/sapadapter-service/Omicron.SapAdapter.Services/Sap/AdvanceLookService.cs
@@ -11,7 +11,6 @@ namespace Omicron.SapAdapter.Services.Sap
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Security.Cryptography.X509Certificates;
     using System.Text;
     using System.Threading.Tasks;
     using Newtonsoft.Json;
