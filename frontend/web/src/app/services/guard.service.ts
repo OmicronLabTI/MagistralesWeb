de8b2cf357e3659f4fddd4e755ccf8c2283a6838
Daniel <daniel.vargassanchez@axity.com>
2023-02-17 09:30:05 -0600

    OM-1234, FEATURE
    Solutions:
    * Codesmell

--- frontend/web/src/app/services/guard.service.ts
+++ frontend/web/src/app/services/guard.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import {CONST_NUMBER, pathRoles, RolesType} from '../constants/const';
+import { CONST_NUMBER, pathRoles, RolesType } from '../constants/const';
 import { DataService } from './data.service';
 import { LocalStorageService } from './local-storage.service';
 
@@ -13,47 +13,38 @@ export class GuardService implements CanActivate {
     private dataService: DataService,
     private router: Router,
     private localStorageService: LocalStorageService
-    ) {
+  ) {
   }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    let pass = false;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const pass = false;
+    const path = route.url[CONST_NUMBER.zero].path;
     if (this.localStorageService.userIsAuthenticated()) {
       switch (this.localStorageService.getUserRole()) {
         case RolesType.admin:
-          pass = pathRoles.admin.includes(route.url[CONST_NUMBER.zero].path);
-          if (!pass) {
-            this.router.navigate([pathRoles.admin[CONST_NUMBER.zero]]);
-            return pass;
-          }
-          break;
+          return this.validateAndReturn(path, pathRoles.admin, [pathRoles.admin[CONST_NUMBER.zero]]);
         case RolesType.logistic:
-          pass = pathRoles.logistica.includes(route.url[CONST_NUMBER.zero].path);
-          if (!pass) {
-            this.router.navigate([pathRoles.logistica[CONST_NUMBER.zero]]);
-            return pass;
-          }
-          break;
+          return this.validateAndReturn(path, pathRoles.logistica, [pathRoles.logistica[CONST_NUMBER.zero]]);
         case RolesType.warehouse:
         case RolesType.design: // role design === role warehouse
-          pass = pathRoles.design.includes(route.url[CONST_NUMBER.zero].path);
-          if (!pass) {
-            this.router.navigate([pathRoles.design[CONST_NUMBER.zero]]);
-            return pass;
-          }
-          break;
+          return this.validateAndReturn(path, pathRoles.design, [pathRoles.design[CONST_NUMBER.zero]]);
         case RolesType.incidents:
-          pass = pathRoles.incidents.includes(route.url[CONST_NUMBER.zero].path);
-          if (!pass) {
-            this.router.navigate([pathRoles.incidents[CONST_NUMBER.zero]]);
-            return pass;
-          }
-          break;
+          return this.validateAndReturn(path, pathRoles.incidents, [pathRoles.incidents[CONST_NUMBER.zero]]);
+        default:
+          return pass;
       }
-      return pass;
     } else {
       this.router.navigate(['login']);
       return pass;
     }
+  }
+
+  validateAndReturn(path: string, rolesList: string[], route: string[]): boolean {
+    const pass = rolesList.includes(path);
+    if (!pass) {
+      this.router.navigate(route);
+      return pass;
+    }
+    return pass;
   }
 }
