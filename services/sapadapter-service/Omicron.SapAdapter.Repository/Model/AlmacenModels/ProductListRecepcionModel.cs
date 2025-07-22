3c737788f256359953bf39fa27bc3657c276e424
danielvargassanchezAxity <79526143+danielvargassanchezAxity@users.noreply.github.com>
2025-07-22 12:39:43 -0600

    OM-5107
    Mensaje de productos remisionados parcialmente

--- services/sapadapter-service/Omicron.SapAdapter.Repository/Model/AlmacenModels/ProductListRecepcionModel.cs
+++ services/sapadapter-service/Omicron.SapAdapter.Repository/Model/AlmacenModels/ProductListRecepcionModel.cs
@@ -26,5 +26,11 @@ namespace Omicron.SapAdapter.Entities.Model.AlmacenModels
         /// </summary>
         /// <value>The code.</value>
         public bool HasPendingToStore { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether gets or sets Code.
+        /// </summary>
+        /// <value>The code.</value>
+        public int AssignedPieces { get; set; }
     }
 }
