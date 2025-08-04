ffba771938d591d8e2e59c5e12b8d656861e6c83
JSCastilloH <116733805+JSCastilloH@users.noreply.github.com>
2025-08-04 17:11:05 -0600

    Update OrderDetailViewControllerExtension.swift

--- frontend/ios/Omicron/MVVM/View/OrderDetail/OrderDetailViewControllerExtension.swift
+++ frontend/ios/Omicron/MVVM/View/OrderDetail/OrderDetailViewControllerExtension.swift
@@ -187,7 +187,7 @@ extension OrderDetailViewController {
                 cell.selectedOption = data.warehouse ?? String()
                 cell.delegate = self
                 cell.productId = data.productID ?? String()
-                cell.pickerContainerView.isUserInteractionEnabled = self?.orderDetail[0].orderRelationType != OrderRelationTypes.padre
+                cell.pickerContainerView.isUserInteractionEnabled = self?.orderDetail[0].orderRelationType != OrderRelationTypes.completa
         }.disposed(by: disposeBag)
         orderDetailViewModel.tableData.subscribe(onNext: { [weak self] details in
             guard let self = self else { return }
