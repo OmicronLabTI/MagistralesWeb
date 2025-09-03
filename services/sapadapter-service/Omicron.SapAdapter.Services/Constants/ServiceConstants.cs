1b6103cd59be731f53bedc21d60327b228b85a95
VSanchez8 <victorhugo.sanchez@axity.com>
2025-09-03 13:36:58 -0600

    bugfix/OM-6555
    se excluye el parámetro de las clasificaciones al construir la key de redis

--- services/sapadapter-service/Omicron.SapAdapter.Services/Constants/ServiceConstants.cs
+++ services/sapadapter-service/Omicron.SapAdapter.Services/Constants/ServiceConstants.cs
@@ -1049,6 +1049,7 @@ namespace Omicron.SapAdapter.Services.Constants
             Advance,
             Current,
             NeedsLargeDsc,
+            Classifications,
         };
 
         /// <summary>
