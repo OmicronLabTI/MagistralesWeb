c4d7bc3e3e722f5c2cefbad37dec964778666664
Josué Saul Castillo Hernández <josue.castilloh@axity.com>
2025-10-02 10:56:22 -0600

    Update pedido-detalle.component.ts

--- frontend/web/src/app/pages/pedido-detalle/pedido-detalle.component.ts
+++ frontend/web/src/app/pages/pedido-detalle/pedido-detalle.component.ts
@@ -367,7 +367,6 @@ export class PedidoDetalleComponent implements OnInit, OnDestroy {
       FromToFilter.fromOrderIsolatedReassignItems).map(order => Number(order.ordenFabricacionId));
 
     const dataRequest = parentOrdersReasign.concat(childrenOrderToReasign);
-    console.log(dataRequest);
     this.observableService.setQbfToPlace({
       modalType: MODAL_NAMES.placeOrdersDetail,
       list: dataRequest,
