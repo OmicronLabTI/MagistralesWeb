26eecc2805f75f7880c7b5a45ff4fcf3309a2954
jarturoep <jose.espinosa@axity.com>
2025-10-29 13:30:21 -0600

    Se agrega ResultDto y el test de identidades

---
+++ services/invoice-service/Omicron.Invoice.Test/EntitiesTest.cs
@@ -0,0 +1,70 @@
+﻿// <summary>
+// <copyright file="EntitiesTest.cs" company="Axity">
+// This source code is Copyright Axity and MAY NOT be copied, reproduced,
+// published, distributed or transmitted to or stored in any manner without prior
+// written consent from Axity (www.axity.com).
+// </copyright>
+// </summary>
+
+using AutoFixture;
+using Omicron.Invoice.Common.DTOs.Responses.Common;
+
+namespace Omicron.Invoice.Test
+{
+    /// <summary>
+    /// Class FavoritiesServiceTest.
+    /// </summary>
+    [TestFixture]
+    public class EntitiesTest
+    {
+        /// <summary>
+        /// TypeCases.
+        /// </summary>
+        private static readonly object[] TypeCases =
+        {
+            new ResultDto(),
+        };
+
+        /// <summary>
+        /// The fixture.
+        /// </summary>
+        private readonly Fixture fixture = new Fixture();
+
+        /// <summary>
+        /// Validate instance for type.
+        /// </summary>
+        /// <typeparam name="T">Generic type.</typeparam>
+        /// <param name="instance">Type.</param>
+        /// [AutoData]
+        [Test]
+        [TestCaseSource("TypeCases")]
+        public void TestInstance<T>(T instance)
+            where T : class
+        {
+            this.fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+            this.fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            // Arrange
+            instance = this.fixture.Create<T>();
+
+            // Assert
+            Assert.That(IsValid(instance));
+        }
+
+        private static bool IsValid<T>(T instance)
+        {
+            Assert.That(instance, Is.Not.Null);
+            foreach (var prop in instance.GetType().GetProperties())
+            {
+                Assert.That(GetPropValue(instance, prop.Name), Is.Not.Null);
+            }
+
+            return true;
+        }
+
+        private static object GetPropValue(object src, string propName)
+        {
+            return src.GetType().GetProperty(propName).GetValue(src, null);
+        }
+    }
+}
